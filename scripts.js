3f1c2792639b9f3e9f7bdc220c8bf2a8a9b784c9
athomas901 <athomas.prsvr@gmail.com>
2024-04-29 21:40:52 -0500

    BlackJack

--- scripts.js
+++ scripts.js
@@ -16,4 +16,49 @@ if (sum <= 20) {
     console.log("You've got BlackJack")
 }else {
     console.log("You're out of the game!")
+    isAlive =false
 }
+
+//04/29
+//1. Create a variable called isAlive and assign it to true
+
+let isAlive = True
+
+
+
+//2. Flip its value to false in the appropiate code block
+
+let isAlive = False
+
+console.log(4 === 3)// False
+console.log(5 > 2) //True
+console.log(12 > 12)//False
+console.log(3 < 0) //False
+console.log(3 >= 3)//True
+console.log(11 <= 11) //True
+console.log(3 <= 2) //False
+
+let firstCard = 10
+let secondCard = 7
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+//Declare a variable called message and assign its value to an empty string
+let message = ""
+
+//Reassign the message variable  to the string we're logging out
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card"
+}else if (sum ===21) {
+    message ="Wohoo! You've got Blackjack!"
+    hasBlackJack = true
+}else {
+    message = "You're out of the game!"
+    isAlive = false
+}
+
+//log it out
+console.log(message)
